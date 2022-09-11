cf158ae88d65c7eea96ca9dbb74128c55e2622e2
miiin_sseong <wsc03002@naver.com>
2022-09-12 03:29:18 +0900

    update to ToasterComponent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import Header from "../components/commons/layout/header";
 import { SessionProvider } from "next-auth/react";
 import { ApolloProvider } from "@apollo/client";
 import client from "../components/utils/apollo/apollo-client";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
+        <Toaster />
         <div className="h-screen overflow-y-scroll bg-slate-200">
           <Header />
           <Component {...pageProps} />
