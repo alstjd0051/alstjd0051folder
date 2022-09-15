e53e176503d7d489c57ab6ea7ff15624494535af
miiin_sseong <wsc03002@naver.com>
2022-09-15 22:40:46 +0900

    Updat to Graphql(Query)

--- components/utils/apollo/graphql/queries.ts
+++ components/utils/apollo/graphql/queries.ts
@@ -9,3 +9,68 @@ export const GET_SUBREDDIT_BY_TOPIC = gql`
     }
   }
 `;
+
+export const GET_ALL_POST = gql`
+  query MyQuery {
+    getPostList {
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      subreddit {
+        id
+        topic
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
+
+export const GET_ALL_POSTS_BY_TOPIC = gql`
+  query MyQuery($topic: String!) {
+    getPostListByTopic(topic: $topic) {
+      body
+      comments {
+        id
+        created_at
+        post_id
+        text
+        username
+      }
+      created_at
+      id
+      image
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      subreddit_id
+      title
+      username
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
