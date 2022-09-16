0c7e0516beeae3815c0aedadeda8900523f45f98
miiin_sseong <wsc03002@naver.com>
2022-09-17 06:06:44 +0900

    update to fetch(query)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,41 @@
+import { useQuery } from "@apollo/client";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import FeedUI from "../components/commons/feed/FeedUI";
+import SubredditRow from "../components/commons/list/SubredditRow";
 import PostBoxUI from "../components/commons/postbox/PostBoxUI";
+import { GET_SUBREDDITS_WITH_LIMIT } from "../components/utils/apollo/graphql/queries";
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+    variables: {
+      limit: 10,
+    },
+  });
+  const subreddits: Subreddit[] = data?.getSubredditListLimit;
   return (
     <div className="max-w-5xl my-7 mx-auto">
-      <Head>
-        <title>Tyler</title>
-      </Head>
       {/* Post Box */}
       <PostBoxUI />
       <div className="flex">
         {/* Feed */}
         <FeedUI />
-        <div></div>
+        <div className="sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+          <p className="text-lg mb-1 p-4 pb-3 font-bold">Top Communities</p>
+          <div>
+            {/* List subreddits */}
+            <div>
+              {subreddits?.map((subreddit, i) => (
+                <SubredditRow
+                  key={subreddit.id}
+                  topic={subreddit.topic}
+                  index={i}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
