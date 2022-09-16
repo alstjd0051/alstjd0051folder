1e02fdfd48a39cf686cccb71e768e82db4f47907
miiin_sseong <wsc03002@naver.com>
2022-09-17 06:05:40 +0900

    Create SubredditRow Component

---
+++ components/commons/list/SubredditRow.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ChevronUpIcon } from "@heroicons/react/outline";
+import AvatarUI from "../avatar/AvatarUI";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
+type Props = {
+  topic: string;
+  index: number;
+};
+
+const SubredditRow = ({ index, topic }: Props) => {
+  const { data: session } = useSession();
+  return (
+    <div className="flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b">
+      <p>{index + 1}</p>
+      <ChevronUpIcon className="h-4 w-4 flex-shrink-0 text-green-400 " />
+      <AvatarUI seed={`/subreddit/${topic}`} />
+      <p className="flex-1 truncate">{topic}</p>
+      <Link href={`/subreddit/${topic}`}>
+        <div className="cursor-pointer rounded-full bg-blue-500 px-3 text-white">
+          View
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default SubredditRow;
