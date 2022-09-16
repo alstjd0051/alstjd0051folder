f97cda3aadd43f4f7d36ce5dd99bf1ddef58f94b
miiin_sseong <wsc03002@naver.com>
2022-09-17 06:05:58 +0900

    update to displayVotes

--- components/commons/post/PostUI.tsx
+++ components/commons/post/PostUI.tsx
@@ -17,6 +17,7 @@ import { toast } from "react-hot-toast";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_ALL_VOTES_BY_POST_ID } from "../../utils/apollo/graphql/queries";
 import { ADD_VOTE } from "../../utils/apollo/graphql/mutations";
+import { totalmem } from "os";
 
 type Props = {
   post: Post;
@@ -44,8 +45,9 @@ const PostUI = ({ post }: Props) => {
     if (vote && isUpvote) return;
     if (vote === false && !isUpvote) return;
     console.log("voting...", isUpvote);
-
-    await addVote({
+    const {
+      data: { insertVote: newVote },
+    } = await addVote({
       variables: {
         post_id: post.id,
         username: session?.user?.name,
@@ -53,17 +55,21 @@ const PostUI = ({ post }: Props) => {
       },
     });
   };
-  useEffect(() => {
+
+  const displayVotes = (data: any) => {
     const votes: Vote[] = data?.getVotesByPostId;
+    const displayNumber = votes?.reduce(
+      (total, vote) => (vote.upvote ? (total += 1) : (total -= 1)),
+      0
+    );
 
-    // Latest vote(as we sorted by newely created first is SQL query)
-    // Note: You could improve this by moving it to the original Query
-    const vote = votes?.find(
-      (vote) => vote.username == session?.user?.name
-    )?.upvote;
+    if (votes?.length === 0) return 0;
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1;
+    }
+    return displayNumber;
+  };
 
-    setVote(vote);
-  }, [data]);
   if (!post)
     return (
       <div className="flex w-full items-center justify-center p-10 text-xl">
@@ -71,7 +77,7 @@ const PostUI = ({ post }: Props) => {
       </div>
     );
   return (
-    <Link href={`/post/${post?.id}`}>
+    <>
       <div className="flex cursor-pointer rounded-md  border border-gray-300 bg-white shadow hover:border hover:border-gray-600 ">
         {/* Votes */}
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400 ">
@@ -81,7 +87,7 @@ const PostUI = ({ post }: Props) => {
               vote && "text-red-400"
             }`}
           />
-          <p className="text-black font-bold text-xs">0</p>
+          <p className="text-black font-bold text-xs">{displayVotes(data)}</p>
           <ArrowDownIcon
             onClick={() => upVote(false)}
             className={`voteButton hover:text-blue-400 ${
@@ -89,54 +95,57 @@ const PostUI = ({ post }: Props) => {
             } `}
           />
         </div>
-        <div className="p-3 pb-1">
-          {/* Header */}
-          <div className="flex items-center space-x-2">
-            <AvatarUI seed={post.subreddit[0]?.topic} />
-            <Link href={`/subreddit/${post.subreddit[0]?.topic}`}>
-              <p className="text-xs text-gray-400">
-                💌&nbsp;
-                <span className="font-bold text-black hover:text-blue-400 hover:underline">
-                  {post.username}
-                </span>
-                <TimeAgo date={post.created_at} />
-              </p>
-            </Link>
-          </div>
 
-          {/* Body */}
-          <div className="py-4">
-            <h2 className="text-xl font-semibold">{post.title}</h2>
-            <p className="mt-2 text-sm font-light">{post.body}</p>
-          </div>
+        <Link href={`/post/${post?.id}`}>
+          <div className="p-3 pb-1">
+            {/* Header */}
+            <div className="flex items-center space-x-2">
+              <AvatarUI seed={post.subreddit[0]?.topic} />
+              <Link href={`/subreddit/${post.subreddit[0]?.topic}`}>
+                <p className="text-xs text-gray-400">
+                  💌&nbsp;
+                  <span className="font-bold text-black hover:text-blue-400 hover:underline">
+                    {post.username}
+                  </span>
+                  <TimeAgo date={post.created_at} />
+                </p>
+              </Link>
+            </div>
 
-          {/* Image */}
-          <img className="w-full" src={post.image} alt="" />
-          {/* Footer */}
-          <div className="flex space-x-4 text-gray-400">
-            <div className="postButton">
-              <ChatAltIcon className="h-6 w-6" />
-              <p className="">{post.comments.length} Comments</p>
+            {/* Body */}
+            <div className="py-4">
+              <h2 className="text-xl font-semibold">{post.title}</h2>
+              <p className="mt-2 text-sm font-light">{post.body}</p>
             </div>
-            <div className="postButton">
-              <GiftIcon className="h-6 w-6" />
-              <p className="hidden sm:inline">{post.comments.length} Award</p>
-            </div>
-            <div className="hidden sm:inlinepostButton">
-              <ShareIcon className="h-6 w-6" />
-              <p className="hidden sm:inline">{post.comments.length} Share</p>
-            </div>
-            <div className="postButton">
-              <BookmarkIcon className="h-6 w-6" />
-              <p className="hidden sm:inline">{post.comments.length} Save</p>
-            </div>
-            <div className="postButton">
-              <DotsHorizontalIcon className="h-6 w-6" />
+
+            {/* Image */}
+            <img className="w-full" src={post.image} alt="" />
+            {/* Footer */}
+            <div className="flex space-x-4 text-gray-400">
+              <div className="postButton">
+                <ChatAltIcon className="h-6 w-6" />
+                <p className="">{post.comments.length} Comments</p>
+              </div>
+              <div className="postButton">
+                <GiftIcon className="h-6 w-6" />
+                <p className="hidden sm:inline">{post.comments.length} Award</p>
+              </div>
+              <div className="hidden sm:inlinepostButton">
+                <ShareIcon className="h-6 w-6" />
+                <p className="hidden sm:inline">{post.comments.length} Share</p>
+              </div>
+              <div className="postButton">
+                <BookmarkIcon className="h-6 w-6" />
+                <p className="hidden sm:inline">{post.comments.length} Save</p>
+              </div>
+              <div className="postButton">
+                <DotsHorizontalIcon className="h-6 w-6" />
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
-    </Link>
+    </>
   );
 };
 
