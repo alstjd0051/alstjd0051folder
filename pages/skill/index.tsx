8ed1de1607ec59c131315d8d36b76634ce205eda
miiin_sseong <wsc03002@naver.com>
2022-09-18 01:49:22 +0900

    create:: SkillPage

--- pages/skill/index.tsx
+++ pages/skill/index.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const SkillPage = () => {
   return (
     <div className="max-w-5xl my-7 mx-auto">
-      <h1>추후 공계예정</h1>
+      <Link href={"https://minseongfolio2022-2d8e6.web.app/"}>
+        <div className="flex flex-col cursor-pointer">
+          <h1 className="font-bold text-2xl text-blue-400">React</h1>
+          <p>Click To Here</p>
+        </div>
+      </Link>
     </div>
   );
 };
